ec592badc941f849c23a976f98431b10e2da4011
udhaindah <udhaindah937@gmail.com>
2025-01-12 11:17:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const BASE_URL = 'https://api.depined.org/api';
 
 // Fungsi untuk menampilkan banner ASCII art
 const displayBanner = () => {
-  console.log(chalk.green(figlet.textSync('AirdropInsiders', { horizontalLayout: 'default' })));
+  console.log(chalk.green(figlet.textSync('🄱🅂🄼', { horizontalLayout: 'default' })));
 };
 
 // Format timestamps
@@ -245,8 +245,8 @@ const main = async () => {
   while (true) {
     console.clear();
     displayBanner(); // Tampilkan banner sebelum setiap refresh tabel
-    console.log(chalk.yellow('Join Us : https://t.me/AirdropInsiderID\n'));
-    console.log(chalk.cyan('=== Depined Multi-Account Manager ===\n'));
+    console.log(chalk.yellow('Bright Spot Min\n'));
+    console.log(chalk.cyan('=== Menyala Abangku ===\n'));
     console.log(createStatsTable(accounts).toString());
     console.log(chalk.cyan('\n=== Activity Log ==='));
 
